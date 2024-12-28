4f0c2afee6314aa70c3f609fdfb3f14ebd5f4efb
pery <oluwapery99@gmail.com>
2024-12-28 23:49:31 +0100

    fixed error

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-
+    const token = req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
-      return res.status(401).json({ error: "Please provide authentication token" });
+      return res
+        .status(401)
+        .json({ error: "Please provide authentication token" });
     }
-
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { _id: verified.id, email: verified.email };
+    req.user = { _id: verified.id };
     next();
   } catch (error) {
     res.status(401).json({ error: "Invalid token" });
